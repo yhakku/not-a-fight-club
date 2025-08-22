37b2faa0923b972183ab39edb3f07c21ee81375b
yhakku <karginyura13@gmail.com>
2025-08-22 14:14:43 +0300

    feat: implement nickname validity

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,9 @@
 const initLogin = () => {
   const title = document.querySelector('.login__title');
   const form = document.querySelector('.form');
+  const formContainer = document.querySelector('.form__form-container');
   const input = document.querySelector('.form__input');
-  const buttonSubmit = document.querySelector('.form__submit');
+  const formError = document.querySelector('.form__error');
   const buttonChars = document.querySelector('.login__button-chars');
   const buttonSettings = document.querySelector('.login__button-settings');
 
@@ -51,12 +52,29 @@ const initLogin = () => {
   };
 
   if (nickname === null) {
+    formContainer.style.paddingBottom = '10px';
     buttonChars.classList.add('hidden');
     buttonSettings.classList.add('hidden');
   } else {
     form.classList.add('hidden');
     createElementP(nickname);
   }
+
+  input.addEventListener('input', () => {
+    const validityState = input.validity;
+    console.log(validityState);
+
+    input.setCustomValidity(' ');
+
+    if (validityState.patternMismatch) {
+      input.classList.add('invalid');
+      formError.classList.add('invalid');
+    } else {
+      input.classList.remove('invalid');
+      formError.classList.remove('invalid');
+      input.setCustomValidity('');
+    }
+  });
 };
 
 export default initLogin;
