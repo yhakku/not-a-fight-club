9eb2a91001d2ec56cc059bf39275fd6d59208295
yhakku <karginyura13@gmail.com>
2025-08-25 19:33:38 +0300

    feat: implement close characters modal

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,10 +1,15 @@
 import { loadState } from './state';
 
 const initChars = () => {
+  const body = document.querySelector('body');
+  const overlay = document.querySelector('.overlay');
+  const logo = document.querySelector('.logo');
+  const buttonClose = document.querySelector('.characters__button-close');
   const nicknames = document.querySelectorAll('.characters__nickname');
   const wins = document.querySelectorAll('.characters__result-w');
   const loses = document.querySelectorAll('.characters__result-l');
   const selectedChars = document.querySelectorAll('.characters__selected');
+  const settingsLink = document.querySelector('a[href="#settings"]');
 
   const data = loadState();
 
@@ -38,8 +43,40 @@ const initChars = () => {
         data.user.avatarId = index;
       });
     });
-  } else {
-    location.hash = 'login';
+  }
+
+  let prevHash = null;
+  let currentHash = location.hash;
+
+  window.addEventListener('hashchange', () => {
+    prevHash = currentHash;
+    currentHash = location.hash;
+
+    if (currentHash === '#characters') {
+      settingsLink.classList.add('disabled');
+    } else {
+      settingsLink.classList.remove('disabled');
+    }
+  });
+
+  const closeModal = () => {
+    body.classList.remove('overlay');
+    logo.classList.remove('hidden');
+
+    if (prevHash === '#settings') {
+      history.go(-2);
+    } else {
+      history.back();
+    }
+  };
+
+  if (buttonClose) {
+    buttonClose.addEventListener('click', closeModal);
+  }
+  if (overlay) {
+    overlay.addEventListener('click', (event) => {
+      if (event.target === overlay) closeModal();
+    });
   }
 };
 
