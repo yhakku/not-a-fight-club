73f6be123b6a1992ca234644d8fda80c0b97fd05
yhakku <karginyura13@gmail.com>
2025-12-09 18:01:51 +0300

    fix: add logs for search error

--- src/js/ui.js
+++ src/js/ui.js
@@ -37,6 +37,10 @@ export function updateStatsUI() {
   const wins = document.querySelectorAll('.characters__result-w');
   const loses = document.querySelectorAll('.characters__result-l');
 
+  console.log('characters:', state.characters);
+  console.log('win elements:', wins);
+  console.log('lose elements:', loses);
+
   if (!state.characters || !Array.isArray(state.characters)) return;
 
   state.characters.forEach((char, index) => {
